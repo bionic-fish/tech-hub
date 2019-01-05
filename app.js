13672a18b305ab85fb730b4fd2db1aad25339ebb
Chris <c.d.harris.dedoc@gmail.com>
2019-01-05 18:14:15 +1100

    TechHub Creation

---
+++ app.js
@@ -0,0 +1,8 @@
+// google maps API
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById("maps"), {
+        center: {lat: -37.813, lng: 144.96},
+        zoom: 10
+    });
+}
